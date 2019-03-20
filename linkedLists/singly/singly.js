c5ace9154c7d7f9effe4e2759785ccc4ba385802
seintun <seintun.edu@gmail.com>
2019-03-20 15:32:03 -0700

    Added push(val) to singlyLL

--- linkedLists/singly/singly.js
+++ linkedLists/singly/singly.js
@@ -11,7 +11,21 @@ class singlyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 let list = new singlyLinkedList();
+list.push("Hello");
+list.push("Goodbye");
 console.log(list);
