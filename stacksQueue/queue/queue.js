f9afbefd7e681fa4f86c3799be96c05ed994f6f8
seintun <seintun.edu@gmail.com>
2019-03-20 16:27:10 -0700

    Create queue list and added enqueue

--- stacksQueue/queue/queue.js
+++ stacksQueue/queue/queue.js
@@ -11,7 +11,24 @@ class Queue {
     this.last = null;
     this.size = 0;
   }
+  enqueue(value) {
+    var newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return this.size++;
+  }
 }
 
+// Create a new queue list
 let queue = new Queue();
+console.log(queue);
+
+// Adding node/print jobs in a queue ["Print One", "Print Two"]
+queue.enqueue("Print One");
+queue.enqueue("Print Two");
 console.log(queue);
