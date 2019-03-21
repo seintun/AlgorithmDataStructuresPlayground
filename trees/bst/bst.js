66db560208ab7992651672dc05722decae8ad30b
seintun <seintun.edu@gmail.com>
2019-03-20 17:42:30 -0700

    insert new node for BST

--- trees/bst/bst.js
+++ trees/bst/bst.js
@@ -10,11 +10,47 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          }
+          current = current.right;
+        }
+      }
+    }
+  }
 }
 
 let tree = new BinarySearchTree();
 console.log(tree);
-tree.root = new Node("15");
-tree.root.right = new Node("5");
-tree.root.left = new Node("20");
+// tree.root = new Node("15");
+// tree.root.right = new Node("5");
+// tree.root.left = new Node("20");
+// console.log(tree);
+
+tree.insert(10);
+console.log(tree);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
 console.log(tree);
