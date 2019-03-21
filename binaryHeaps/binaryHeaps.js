4fc5a5dcf54da4eb0e53711eca8fe79e1f09d46e
seintun <seintun.edu@gmail.com>
2019-03-20 23:17:00 -0700

    Added insert for MaxBinaryHeap

--- binaryHeaps/binaryHeaps.js
+++ binaryHeaps/binaryHeaps.js
@@ -2,8 +2,9 @@
 // Always insert and fill-up the left most of the tree
 // Bubble-up, when new node inserted is larger than parent, keep swapping up
 // [41, 39, 55, 18, 27, 12, 33]
+// [01, 02, 03, 04, 05, 06, 07]
 // Finding child node @ 2n+1 for left child node and 2n+2 for right child node
-// Finding parent node @ (n-1)/2 from left child node and (n-2)/2 from right child node after Math.floor
+// Finding parent node @ (n-1)/2 after Math.floor
 /**
  *          41
  *        /    \
@@ -11,3 +12,30 @@
  *     /  \    /  \
  *    18  27  12  33
  */
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [41, 39, 55, 18, 27, 12];
+  }
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+}
+
+let heap = new MaxBinaryHeap();
+console.log(heap);
+heap.insert(111);
+console.log(heap);
