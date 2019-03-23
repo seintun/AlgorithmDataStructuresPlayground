f0a92b53961633116e1097d9ab3927adf8c11d6c
seintun <seintun.edu@gmail.com>
2019-03-23 14:53:27 -0700

    Test removing edges between SF-Tokyo

--- graphs/graph.js
+++ graphs/graph.js
@@ -9,6 +9,10 @@ class Graph {
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
   }
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
+  }
 }
 
 let graph = new Graph();
@@ -22,4 +26,6 @@ graph.addEdge("Dallas", "New York");
 graph.addEdge("Dallas", "San Francisco");
 graph.addEdge("San Francisco", "New York");
 graph.addEdge("San Francisco", "Tokyo");
-console.log(graph);
+console.log("Added edges for airports", graph);
+graph.removeEdge("San Francisco", "Tokyo");
+console.log("Removed SF-Tokyo edge", graph);
