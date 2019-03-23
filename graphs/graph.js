f94c926df2a8bd0d3b228b019fc91eec58c47c84
seintun <seintun.edu@gmail.com>
2019-03-23 14:49:38 -0700

    Establish edgefor each vertex

--- graphs/graph.js
+++ graphs/graph.js
@@ -5,12 +5,21 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
 }
 
 let graph = new Graph();
-console.log(graph);
+// console.log(graph);
 graph.addVertex("Dallas");
 graph.addVertex("Tokyo");
 graph.addVertex("New York");
 graph.addVertex("San Francisco");
+// console.log(graph);
+graph.addEdge("Dallas", "New York");
+graph.addEdge("Dallas", "San Francisco");
+graph.addEdge("San Francisco", "New York");
+graph.addEdge("San Francisco", "Tokyo");
 console.log(graph);
