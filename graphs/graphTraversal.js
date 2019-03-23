8859930d4e595a67d1fc8ffa05ebeff818892bf4
seintun <seintun.edu@gmail.com>
2019-03-23 16:50:34 -0700

    dfaTraversalRecursively

--- graphs/graphTraversal.js
+++ graphs/graphTraversal.js
@@ -21,26 +21,48 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+  depthFirstRecursive(start) {
+    // result to print of visited vertex
+    const result = [];
+    // hash table to keep track which vertex is visited
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      // return null when missing args
+      if (!vertex) return null; // base case
+      // set each vertex as true when visted in hash table
+      visited[vertex] = true;
+      // push each visited vertex to result
+      result.push(vertex);
+      // iteriate each value of corresponding vertex
+      adjacencyList[vertex].forEach(neighbor => {
+        // if the value vertex yet visted, recursively pass as args
+        if (!visited[neighbor]) return dfs(neighbor);
+      });
+    })(start);
+    console.log(result);
+    return result;
+  }
 }
 
 let g = new Graph();
 
-g.addVertex("A")
-g.addVertex("B")
-g.addVertex("C")
-g.addVertex("D")
-g.addVertex("E")
-g.addVertex("F")
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
 
-
-g.addEdge("A", "B")
-g.addEdge("A", "C")
-g.addEdge("B","D")
-g.addEdge("C","E")
-g.addEdge("D","E")
-g.addEdge("D","F")
-g.addEdge("E","F")
-g.depthFirstRecursive("A")
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+g.depthFirstRecursive("A");
 
 //          A
 //        /   \
@@ -49,4 +71,3 @@ g.depthFirstRecursive("A")
 //       D --- E
 //        \   /
 //          F
-
