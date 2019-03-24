5bb4587c2618406e7563550c9af04b19f6a164cc
seintun <seintun.edu@gmail.com>
2019-03-23 19:44:17 -0700

    Create initial hash tables for tracing prev and implement PQ for finding optimal shortest weight

--- dijkstraAlgorithm/dijkstraSimplePQ.js
+++ dijkstraAlgorithm/dijkstraSimplePQ.js
@@ -1,18 +1,4 @@
 // Create a mock Dijkstra
-
-class WeightedGraph {
-  constructor() {
-    this.adjacencyList = {};
-  }
-  addVErtex(vertex) {
-    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-  }
-  addEdge(vertex1, vertex2, weight) {
-    this.adjacencyList[vertex1].push({ node: vertex2, weight });
-    this.adjacencyList[vertex2].push({ node: vertex1, weight });
-  }
-}
-
 class PriorityQueue {
   constructor() {
     this.values = [];
@@ -25,6 +11,41 @@ class PriorityQueue {
   }
   sort() {
     this.values.sort((a, b) => a.priority - b.priority);
+  }
+}
+
+class WeightedGraph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+  addVErtex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2, weight) {
+    this.adjacencyList[vertex1].push({ node: vertex2, weight });
+    this.adjacencyList[vertex2].push({ node: vertex1, weight });
+  }
+  // add Dijkstra Algorithm to find the shortest path/weight from start to finish
+  Dijkstra(start, finish) {
+    const nodes = new PriorityQueue();
+    const distances = {};
+    const previous = {};
+    let path = []; // return at the end
+    let smallest;
+    //  building up the initial state
+    for (let vertex in this.adjacencyList) {
+      // if the vertex is the start, assign as 0 and add to PQ list
+      if (vertex === start) {
+        distances[vertex] = 0;
+        nodes.enqueue(vertex, 0);
+      } else {
+        // set the initial weight as Infinity since its distance is unknown
+        distances[vertex] = Infinity;
+        nodes.enqueue(vertex, Infinity);
+      }
+      // intiate each vertex prev as null to be assigned later
+      previous[vertex] = null;
+    }
   }
 }
 
